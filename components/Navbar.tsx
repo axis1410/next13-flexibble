c2b2cf26fb0670677916941b0698ea804265aa43
axis.1410 <adityasingh14102001@gmail.com>
2023-06-27 20:57:37 +0530

    Created endpoints

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,11 @@ import { NavLinks } from '@/constant';
 import Image from 'next/image';
 import Link from 'next/link';
 import AuthProviders from './AuthProviders';
+import { getCurrentUser } from '@/lib/session';
 
-const Navbar = () => {
-	const session = {};
-	// const session = null;
+const Navbar = async () => {
+	// const session = {};
+	const session = await getCurrentUser();
 
 	return (
 		<nav className='flexBetween navbar'>
@@ -30,9 +31,18 @@ const Navbar = () => {
 				</ul>
 			</div>
 			<div className='flexCenter gap-4'>
-				{session ? (
+				{session?.user ? (
 					<>
-						UserPhoto
+						{session.user?.image && (
+							<Image
+								src={session.user.image}
+								width={40}
+								height={40}
+								alt={session.user.name}
+								className='rounded-full'
+							/>
+						)}
+
 						<Link href='/'>Share work</Link>
 					</>
 				) : (
