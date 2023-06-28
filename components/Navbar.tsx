ea14589e7b567085afc4e5bd488891497710c8b9
axis.1410 <adityasingh14102001@gmail.com>
2023-06-28 10:05:18 +0530

    Implemented Profile Menu component and headlessui

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import AuthProviders from './AuthProviders';
 import { getCurrentUser } from '@/lib/session';
+import { signOut } from 'next-auth/react';
+import ProfileMenu from './ProfileMenu';
 
 const Navbar = async () => {
 	// const session = {};
@@ -33,15 +35,7 @@ const Navbar = async () => {
 			<div className='flexCenter gap-4'>
 				{session?.user ? (
 					<>
-						{session.user?.image && (
-							<Image
-								src={session.user.image}
-								width={40}
-								height={40}
-								alt={session.user.name}
-								className='rounded-full'
-							/>
-						)}
+						<ProfileMenu session={session} />
 
 						<Link href='/'>Share work</Link>
 					</>
